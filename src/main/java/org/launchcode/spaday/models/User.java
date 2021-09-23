7741eb2166d1e586aa9078940d59a5d775ef6e19
Khadija Bano <kbano88@gmail.com>
2021-09-22 20:23:07 -0500

    finished Studio (need to do bonus)

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,34 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min=5, max=15, message="Username should at least be 5 characters long and maximum 15 characters")
     private String username;
+
+    @Email(message = "Try Again")
     private String email;
+
+    @NotBlank(message="Password is required")
+    @Size(min=6, message = "Password must be at least 6 characters long")
     private String password;
 
-    public User() {
+//    @NotNull(message = "Passwords do not match")
+//    private String verify;
 
-    }
+    public User() {}
 
     public User(String username, String email, String password) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+
     }
 
     public String getUsername() {
@@ -39,4 +54,6 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
 }
